4a9e527509505a4e12a9435d6485f26c97e6104c
Abel Valle Chavez <abel.valle@globant.com>
2020-03-21 21:37:51 -0600

    Add of CSV data source file.

--- poverty_data_generator.py
+++ poverty_data_generator.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-df = pd.read_csv('input/pobreza_lat_long_2015_es.csv', encoding='latin', thousands=',', na_values='n.d')
-df.to_json('input/pobreza-lat-lng-2015.json', orient='records')
+df = pd.read_csv('static/pobreza_lat_long_2015_es.csv', encoding='latin', thousands=',', na_values='n.d')
+df.to_json('static/pobreza-lat-lng-2015.json', orient='records')
